33ad2315b1518d37b5142847e0ac0acda50b2461
David-Shibley <davidjshibley@gmail.com>
2015-11-17 16:11:16 -0700

    complete the browse feature

--- app.js
+++ app.js
@@ -1,57 +1,80 @@
-
-
 $(document).ready(function() {
+  // Setting the television screen as the canvas element
   var canvas = document.getElementById('canvas');
+  // Define the demensions of the canvas
   canvas.height = 300;
   canvas.width = 300;
   var context = null;
-  var backgroundPic = new Image();
+  // Check to make sure canvas is supported
   if (canvas.getContext) {
     context = canvas.getContext('2d');
   }  else {
-    alert('Canvas not supported');
+    alert('Canvas not supported, please make sure you are using the latest version of your browser.');
   }
-
-  // $.get('https://api.shutterstock.com/v2/images/search?fields=data(id,preview_url)', function(data) {
-
-  var clientID = '1ae470d8641474a7bf10';
+  // API key information
+  var clientId = '1ae470d8641474a7bf10';
   var clientSecret = 'baded4ad3c3894bbc8373de7c31e3eef8bac25db';
-  $.ajax({
-    url: 'https://api.shutterstock.com/v2/images/search?query=*',
-    headers: {
-      Authorization: 'Basic ' + window.btoa(clientID + ':' + clientSecret)
-    }
-  }).done(function(data) {
-    dataStuff = data;
-    console.log(dataStuff);
-  })
+  // The call to shutterstock server
+  var createBrowseImages = function() {
+      $.ajax({
+      url: 'https://api.shutterstock.com/v2/images/search?query=',
+      headers: {
+        Authorization: 'Basic ' + window.btoa(clientId + ':' + clientSecret)
+      }
+    }).done(function(data) {
+      // What to do with the data sent back by shutterstock
+      for (var i = 0; i < data.data.length; i++) {
+        var returnData = data.data[i];
+        var previewData = returnData.assets.preview;
+        var dataUrl = previewData.url;
+        var $imageTemplate = $("<img src=\"" + dataUrl + "\" data-url=\"" + dataUrl + "\"/>");
+        $imageTemplate.click(function() {
+          setImageURL(this.dataset.url);
+        })
 
+        $('#browse').append($imageTemplate);
+      }
+      // console.log(data);
+    });
+  };
   var base64img = canvas.toDataURL();
-  console.log(base64img);
+  // console.log(base64img);
 
   var televisionHeight = $('#canvas').height();
   var televisionWidth = $('#canvas').width();
   var imgFile = null;
   var imgURL = null;
-  var paintPricture = function(event) {
+  var browseSelected = false;
+
+  var paintPicture = function(event) {
     if($('#input_file')[0].files.length > 0){
       imgFile = $('#input_file')[0].files[0];
       var fileReader = new FileReader();
       fileReader.onload = function() {
-        console.log(fileReader.result);
         var processedImage = imgprocess(fileReader.result, true, true, 8);
-        console.log(processedImage);
         setImageURL(processedImage);
       }
       fileReader.readAsDataURL(imgFile);
-    } else {
+    // } else if ($('img').hasClass('selected')) {
+    //   imgURL = $('.selected').data('url');
+    //   console.log(imgURL);
+    //   setImageURL(imgURL);
+    }
+
+    else {
       imgURL = $('#input_url').val();
       setImageURL(imgURL);
     }
     event.preventDefault();
   };
 
-  $('button').click(paintPricture);
+  // $('img').click(function() {
+  //   console.log('selected');
+  //   $(this).toggleClass('selected');
+  // })
+
+  $('button').click(paintPicture);
+  $('.make-pic').click(createBrowseImages);
 
 
   function setImageURL(url) {
@@ -63,7 +86,5 @@ $(document).ready(function() {
       context.drawImage(imageObj, 0, 0, televisionWidth, televisionHeight);
     };
     imageObj.src = url;
-
-    console.log(televisionWidth);
   }
 });
